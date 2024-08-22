f17d357ca4245983585e873489d1a82bdeba56e0
phuongtq <tranphuong.080@gmail.com>
2024-08-22 14:18:11 +0700

    build: correct module build

--- module/contrib/local/protocgen.sh
+++ module/contrib/local/protocgen.sh
@@ -2,31 +2,30 @@
 
 set -eo pipefail
 
-if [ -z $GOPATH ]; then
-	echo "GOPATH not set!"
-	exit 1
-fi
+# if [ -z $GOPATH ]; then
+# 	echo "GOPATH not set!"
+# 	exit 1
+# fi
 
-if [[ $PATH != *"$GOPATH/bin"* ]]; then
-	echo "GOPATH/bin must be added to PATH"
-	exit 1
-fi
+# if [[ $PATH != *"$GOPATH/bin"* ]]; then
+# 	echo "GOPATH/bin must be added to PATH"
+# 	exit 1
+# fi
 
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   buf protoc \
-  -I "proto" \
-  -I "third_party/proto" \
-  --gocosmos_out=plugins=interfacetype+grpc,\
-Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
+    -I "proto" \
+    -I "third_party/proto" \
+    --gocosmos_out=plugins=interfacetype+grpc,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
+    $(find "${dir}" -maxdepth 1 -name '*.proto')
 
   # # command to generate gRPC gateway (*.pb.gw.go in respective modules) files
   buf protoc \
-  -I "proto" \
-  -I "third_party/proto" \
-  --grpc-gateway_out=logtostderr=true,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
+    -I "proto" \
+    -I "third_party/proto" \
+    --grpc-gateway_out=logtostderr=true,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
+    $(find "${dir}" -maxdepth 1 -name '*.proto')
 
 done
 
