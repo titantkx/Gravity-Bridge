b4eb50255e0d137126731ec8753546a8d524937c
phuongtq <tranphuong.080@gmail.com>
2024-10-28 13:50:48 +0700

    test: config evm, titan docker node

---
+++ tests/compose/titan/setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -eux
+
+# Detect platform
+platform=$(uname)
+if [ "$platform" = "Darwin" ]; then
+  SED_INPLACE="sed -i ''"
+else
+  SED_INPLACE="sed -i"
+fi
+
+# your gaiad binary name
+BIN=/titan/bin/titand
+
+ls -alh /titan
+
+VALIDATOR_HOME="/root/.titand"
+CHAIN_ID="titan_18887-1"
+DENOM="atkx"
+ALLOCATION="10000000000000000000000000000$DENOM"
+
+### init chain
+$BIN init --home $VALIDATOR_HOME --chain-id $CHAIN_ID val-1 --default-denom $DENOM
+
+### config node
+$SED_INPLACE 's/^indexer = ".*"/indexer = "kv"/' $VALIDATOR_HOME/config/config.toml
+$SED_INPLACE 's/^timeout_commit = ".*"/timeout_commit = "0.5s"/' $VALIDATOR_HOME/config/config.toml
+
+$SED_INPLACE '/^\[api\]$/,/^\[/ s/^\(enable = \).*/\1true/' $VALIDATOR_HOME/config/app.toml
+$SED_INPLACE '/^\[api\]$/,/^\[/ s/^\(swagger = \).*/\1true/' $VALIDATOR_HOME/config/app.toml
+$SED_INPLACE '/^\[api\]$/,/^\[/ s/^\(address = \).*/\1\"tcp:\/\/0.0.0.0:1317\"/' $VALIDATOR_HOME/config/app.toml
+
+### config genesis
+
+# config denom
+jq '.app_state.evm.params.evm_denom = "atkx"' $VALIDATOR_HOME/config/genesis.json >/denom-edited-genesis.json
+
+# a 60 second voting period to allow us to pass governance proposals in the tests
+jq '.app_state.gov.voting_params.voting_period = "60s"' /denom-edited-genesis.json >/gov-edited-genesis.json
+
+mv /gov-edited-genesis.json $VALIDATOR_HOME/config/genesis.json
+
+### config validator
+$BIN keys add --home $VALIDATOR_HOME --keyring-backend test val-1 2>>/validator-phrases
+
+VALIDATOR_KEY=$($BIN keys show val-1 -a --home $VALIDATOR_HOME --keyring-backend test)
+
+$BIN add-genesis-account --home $VALIDATOR_HOME $VALIDATOR_KEY $ALLOCATION
+
+$BIN gentx --home $VALIDATOR_HOME --keyring-backend test val-1 2048tkx
+
+$BIN collect-gentxs --home $VALIDATOR_HOME
---
+++ tests/compose/titan/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eux
+
+# your gaiad binary name
+BIN=/titan/bin/titand
+
+VALIDATOR_HOME="/root/.titand"
+CHAIN_ID="titan_18887-1"
+DENOM="atkx"
+RPC_ADDRESS="--rpc.laddr tcp://0.0.0.0:27657"
+GRPC_ADDRESS="--grpc.address 0.0.0.0:9190"
+# Must remap the grpc-web address because it conflicts with what we want to use
+GRPC_WEB_ADDRESS="--grpc-web.address 0.0.0.0:9192"
+
+LOG_LEVEL="--log_level info"
+
+ARG="--home $VALIDATOR_HOME $RPC_ADDRESS $GRPC_ADDRESS $GRPC_WEB_ADDRESS $LOG_LEVEL"
+
+# shellcheck disable=SC2086
+$BIN $ARG start
