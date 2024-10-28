ac419fde53feb538269738c7d2aaecb76748596e
phuongtq <tranphuong.080@gmail.com>
2024-10-28 18:03:10 +0700

    test(compose): correct ENV_SET

--- tests/compose/orchestrator/setup.sh
+++ tests/compose/orchestrator/setup.sh
@@ -19,7 +19,7 @@ cp -rf shared/validator-eth-keys /validator-eth-keys
 # deploy the ethereum contracts
 pushd /gravity/orchestrator/test_runner
 # shellcheck disable=SC2089
-ENV_SET='DEPLOY_CONTRACTS=1 RUST_BACKTRACE=full NO_GAS_OPT=1 RUST_LOG="INFO,relayer=DEBUG,orchestrator=DEBUG"'
+ENV_SET='DEPLOY_CONTRACTS=1 RUST_BACKTRACE=full NO_GAS_OPT=1 RUST_LOG=INFO,relayer=DEBUG,orchestrator=DEBUG'
 # TEST_TYPE=$TEST_TYPE
 ENV_SET="$ENV_SET COSMOS_NODE_GRPC=http://gravity:9090"
 ENV_SET="$ENV_SET IBC_NODE_GRPC=http://titan:9190"
