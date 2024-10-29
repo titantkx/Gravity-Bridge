18ecf48a25177e93e605684a1c9992a393f47d2d
phuongtq <tranphuong.080@gmail.com>
2024-10-29 09:27:05 +0700

    test(compose); remove track test process file

--- orchestrator/test_runner/src/bootstrapping.rs
+++ orchestrator/test_runner/src/bootstrapping.rs
@@ -208,6 +208,8 @@ pub async fn deploy_contracts(contact: &Contact) {
     }
     let mut file = File::create("/contracts").unwrap();
     file.write_all(&output.stdout).unwrap();
+    let mut file_shared = File::create("/shared/contracts").unwrap();
+    file_shared.write_all(&output.stdout).unwrap();
 }
 
 pub struct BootstrapContractAddresses {
