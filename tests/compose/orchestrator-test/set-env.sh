92fc48deca9051d46f5ab32390a092f71df57c29
phuongtq <tranphuong.080@gmail.com>
2024-10-30 18:12:05 +0700

    test(compose): correct env for test

--- tests/compose/orchestrator-test/set-env.sh
+++ tests/compose/orchestrator-test/set-env.sh
@@ -2,9 +2,11 @@
 
 export COSMOS_NODE_GRPC=http://gravity:9090
 export COSMOS_NODE_ABCI=http://gravity:26657
-export IBC_NODE_GRPC=http://titan:9190
-export IBC_NODE_ABCI=http://titan:27657
+export IBC_NODE_GRPC=http://titan:9090
+export IBC_NODE_ABCI=http://titan:26657
 export ETH_NODE=http://evm:8545
+export IBC_ADDRESS_PREFIX=titan
+export IBC_STAKING_TOKEN=atkx
 
 export RUST_BACKTRACE=full
 export NO_GAS_OPT=1
